2c50c31bb335baec7cc5cb3a6ed6db4284c71442
Giczy Gergő Ákos <akos.gergo@giczy.hu>
2024-05-27 17:20:20 +0000

    palindrome_checker test 2

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,22 +1,24 @@
-with open('./input.txt', 'r') as f:
+# palindrome_checker
+with open('palindrome_checker/input.txt', 'r') as f:
   input = f.read()
-
 
 item_list = input.splitlines()
 
 results = {}
 for item in item_list:
-
+	# filter and count alfanumeric characters
 	alphanum_chars = ''
 	for char in item:
 		if char.isalnum():
 			alphanum_chars += char.lower()
 	unique_count = len(set(alphanum_chars))
 
+	# checl if palindrome
 	if alphanum_chars == alphanum_chars[::-1]:
 		results[item] = ("YES", unique_count)
 	else:
 		results[item] = ("NO", -1)
 
+# print results
 for palindrome_result, unique_count in results.values():
 	print(f"{palindrome_result}, {unique_count}")
